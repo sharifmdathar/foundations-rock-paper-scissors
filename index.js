cca4bf0e61cfa5f1129dd3deb15f041f71766caa
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-06-23 20:06:06 +0200

    feat: Display results in webpage instead of console
    - also write logic to end the game after either one of them attains a score of 5
    
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- index.js
+++ index.js
@@ -4,40 +4,60 @@ const rock = "Rock",
 const choices = [rock, paper, scissors];
 const getComputerChoice = () =>
   choices[Math.floor(Math.random() * choices.length)];
+const resultEl = document.querySelector("#result");
+
+const addParaToDiv = (para) => {
+  const paraEl = document.createElement("p");
+  paraEl.textContent = para;
+  resultEl.appendChild(paraEl);
+};
 
 let humanScore = 0,
   computerScore = 0;
+let isGameOver = false;
 
 const playRound = (humanChoice, computerChoice) => {
   if (humanChoice == computerChoice) {
-    console.log("It was a draw. No winners.");
+    addParaToDiv("It was a draw. No winners.");
   } else if (
     (humanChoice == rock && computerChoice == paper) ||
     (humanChoice == paper && computerChoice == scissors) ||
     (humanChoice == scissors && computerChoice == rock)
   ) {
-    console.log(`You lost! ${computerChoice} beats ${humanChoice}.`);
+    addParaToDiv(`You lost! ${computerChoice} beats ${humanChoice}.`);
     computerScore++;
   } else if (
     (humanChoice == rock && computerChoice == scissors) ||
     (humanChoice == paper && computerChoice == rock) ||
     (humanChoice == scissors && computerChoice == paper)
   ) {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+    addParaToDiv(`You win! ${humanChoice} beats ${computerChoice}.`);
     humanScore++;
   }
 };
 
 const playGame = (humanChoice) => {
-    const computerChoice = getComputerChoice();
-    console.log(`You chose ${humanChoice}`);
-    console.log(`Computer chose ${computerChoice}`);
-    playRound(humanChoice, computerChoice);
-    console.log("\n");
-    console.log(`Current Score: You=${humanScore} Computer=${computerScore}`);
+  const computerChoice = getComputerChoice();
+  addParaToDiv(`You chose ${humanChoice}`);
+  addParaToDiv(`Computer chose ${computerChoice}`);
+  playRound(humanChoice, computerChoice);
+  addParaToDiv(`Current Score: You=${humanScore} Computer=${computerScore}`);
+  resultEl.appendChild(document.createElement("hr"));
+  if (humanScore == 5 || computerScore == 5) {
+    const h1 = document.createElement("h1");
+    h1.textContent = "Game has Ended";
+    resultEl.appendChild(h1);
+    const h2 = document.createElement("h2");
+    h2.textContent = humanScore == 5 ? "You have won" : "You lost";
+    resultEl.appendChild(h2);
+    resultEl.appendChild(document.createElement("hr"));
+    isGameOver = true;
+  }
 };
 
-btnElems = document.querySelectorAll("button")
-Array.from(btnElems).forEach(el => {
-  el.onclick = () => playGame(el.textContent)
-})
+btnElems = document.querySelectorAll("button");
+Array.from(btnElems).forEach((el) => {
+  el.onclick = () => {
+    isGameOver ? alert("Game is over") : playGame(el.textContent);
+  };
+});
