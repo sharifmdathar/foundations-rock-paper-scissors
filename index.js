36c9f5b97d27494b67ec87f89ebea740496dfe8e
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-06-23 07:08:51 +0530

    write logic to play a single round
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const choices = ["rock", "paper", "scissors"];
+const rock = "rock",
+  paper = "paper",
+  scissors = "scissors";
+const choices = [rock, paper, scissors];
 const getComputerChoice = () =>
   choices[Math.floor(Math.random() * choices.length)];
 const getHumanChoice = () => {
@@ -7,3 +10,36 @@ const getHumanChoice = () => {
   );
   return choices[humanChoiceIndex];
 };
+
+let humanScore = 0,
+  computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+  if (humanChoice == computerChoice) {
+    console.log("It was a draw. No winners");
+  } else if (humanChoice == rock && computerChoice == paper) {
+    console.log("You lost! Paper beats Rock");
+    computerScore++;
+  } else if (humanChoice == rock && computerChoice == scissors) {
+    console.log("You win! Rock beats Scissors");
+    humanScore++;
+  } else if (humanChoice == paper && computerChoice == rock) {
+    console.log("You win! Rock beats Paper");
+    humanScore++;
+  } else if (humanChoice == paper && computerChoice == scissors) {
+    console.log("You lost! Scissors beats Paper");
+    computerScore++;
+  } else if (humanChoice == scissors && computerChoice == rock) {
+    console.log("You lost! Rock beats Scissors");
+    computerScore++;
+  } else if (humanChoice == scissors && computerChoice == paper) {
+    console.log("You win! Scissors beats Paper");
+    humanScore++;
+  }
+};
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+console.log(`You chose ${humanChoice}`);
+console.log(`Computer chose ${computerChoice}`);
+playRound(humanChoice, computerChoice);
