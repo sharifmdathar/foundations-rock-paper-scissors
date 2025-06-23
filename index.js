3800f2d362f1e4d30e18d458aa334a86dd6582a1
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-06-23 20:23:43 +0200

    feat: Add button to restart the game
    also use strict inequality checks for comparisions to avoid any unexpected bug
    
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- index.js
+++ index.js
@@ -17,19 +17,19 @@ let humanScore = 0,
 let isGameOver = false;
 
 const playRound = (humanChoice, computerChoice) => {
-  if (humanChoice == computerChoice) {
+  if (humanChoice === computerChoice) {
     addParaToDiv("It was a draw. No winners.");
   } else if (
-    (humanChoice == rock && computerChoice == paper) ||
-    (humanChoice == paper && computerChoice == scissors) ||
-    (humanChoice == scissors && computerChoice == rock)
+    (humanChoice === rock && computerChoice === paper) ||
+    (humanChoice === paper && computerChoice === scissors) ||
+    (humanChoice === scissors && computerChoice === rock)
   ) {
     addParaToDiv(`You lost! ${computerChoice} beats ${humanChoice}.`);
     computerScore++;
   } else if (
-    (humanChoice == rock && computerChoice == scissors) ||
-    (humanChoice == paper && computerChoice == rock) ||
-    (humanChoice == scissors && computerChoice == paper)
+    (humanChoice === rock && computerChoice === scissors) ||
+    (humanChoice === paper && computerChoice === rock) ||
+    (humanChoice === scissors && computerChoice === paper)
   ) {
     addParaToDiv(`You win! ${humanChoice} beats ${computerChoice}.`);
     humanScore++;
@@ -43,20 +43,25 @@ const playGame = (humanChoice) => {
   playRound(humanChoice, computerChoice);
   addParaToDiv(`Current Score: You=${humanScore} Computer=${computerScore}`);
   resultEl.appendChild(document.createElement("hr"));
-  if (humanScore == 5 || computerScore == 5) {
+  if (humanScore === 5 || computerScore === 5) {
     const h1 = document.createElement("h1");
     h1.textContent = "Game has Ended";
     resultEl.appendChild(h1);
     const h2 = document.createElement("h2");
-    h2.textContent = humanScore == 5 ? "You have won" : "You lost";
+    h2.textContent = humanScore === 5 ? "You have won" : "You lost";
     resultEl.appendChild(h2);
     resultEl.appendChild(document.createElement("hr"));
     isGameOver = true;
+    const btn = document.createElement("button");
+    btn.textContent = "Restart";
+    btn.onclick = () => location.reload();
+    resultEl.appendChild(btn);
   }
 };
 
-btnElems = document.querySelectorAll("button");
+const btnElems = document.querySelectorAll("button");
 Array.from(btnElems).forEach((el) => {
+  el.disabled = isGameOver;
   el.onclick = () => {
     isGameOver ? alert("Game is over") : playGame(el.textContent);
   };
